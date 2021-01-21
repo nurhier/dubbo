8d1be7bda7833c967badb38ad1ae479f8277522a
nurhier <zhhebut@gmail.com>
2021-01-21 17:06:05 +0800

    ShortestResponseLoadBalance 算法分析

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/ShortestResponseLoadBalance.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/ShortestResponseLoadBalance.java
@@ -25,7 +25,7 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * ShortestResponseLoadBalance
+ * ShortestResponseLoadBalance 最短响应负载均衡，重点是如何计算最短响应时长
  * </p>
  * Filter the number of invokers with the shortest response time of success calls and count the weights and quantities of these invokers.
  * If there is only one invoker, use the invoker directly;
@@ -38,9 +38,9 @@ public class ShortestResponseLoadBalance extends AbstractLoadBalance {
 
     @Override
     protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
-        // Number of invokers
+        // Number of invokers // 服务提供者数量
         int length = invokers.size();
-        // Estimated shortest response time of all invokers
+        // Estimated shortest response time of all invokers // 最短响应时间，临时变量
         long shortestResponse = Long.MAX_VALUE;
         // The number of invokers having the same estimated shortest response time
         int shortestCount = 0;
@@ -58,14 +58,14 @@ public class ShortestResponseLoadBalance extends AbstractLoadBalance {
         // Filter out all the shortest response invokers
         for (int i = 0; i < length; i++) {
             Invoker<T> invoker = invokers.get(i);
-            RpcStatus rpcStatus = RpcStatus.getStatus(invoker.getUrl(), invocation.getMethodName());
+            RpcStatus rpcStatus = RpcStatus.getStatus(invoker.getUrl(), invocation.getMethodName()); // 根据服务提供者url和接口方法名称获取rpc url请求统计信息
             // Calculate the estimated response time from the product of active connections and succeeded average elapsed time.
-            long succeededAverageElapsed = rpcStatus.getSucceededAverageElapsed();
-            int active = rpcStatus.getActive();
-            long estimateResponse = succeededAverageElapsed * active;
+            long succeededAverageElapsed = rpcStatus.getSucceededAverageElapsed(); // 获取成功请求平均耗时
+            int active = rpcStatus.getActive(); // 服务提供者此方法url请求数
+            long estimateResponse = succeededAverageElapsed * active; // 计算活跃请求预计消耗的时长
             int afterWarmup = getWeight(invoker, invocation);
             weights[i] = afterWarmup;
-            // Same as LeastActiveLoadBalance
+            // Same as LeastActiveLoadBalance // 预计消耗时长小于最短响应时长，则保存此invoker下标，并作为第一个可选invoker，权重作为初始权重
             if (estimateResponse < shortestResponse) {
                 shortestResponse = estimateResponse;
                 shortestCount = 1;
@@ -73,7 +73,7 @@ public class ShortestResponseLoadBalance extends AbstractLoadBalance {
                 totalWeight = afterWarmup;
                 firstWeight = afterWarmup;
                 sameWeight = true;
-            } else if (estimateResponse == shortestResponse) {
+            } else if (estimateResponse == shortestResponse) { // 如果预计消耗时长等于最小消耗时长，则保存invoker下标
                 shortestIndexes[shortestCount++] = i;
                 totalWeight += afterWarmup;
                 if (sameWeight && i > 0
@@ -82,10 +82,10 @@ public class ShortestResponseLoadBalance extends AbstractLoadBalance {
                 }
             }
         }
-        if (shortestCount == 1) {
+        if (shortestCount == 1) { // 如果只有一个最短响应的invoker，则直接选中
             return invokers.get(shortestIndexes[0]);
         }
-        if (!sameWeight && totalWeight > 0) {
+        if (!sameWeight && totalWeight > 0) { // 如果有多个最短响应相同的invoker，则根据权重随机选择一个invoker
             int offsetWeight = ThreadLocalRandom.current().nextInt(totalWeight);
             for (int i = 0; i < shortestCount; i++) {
                 int shortestIndex = shortestIndexes[i];
@@ -95,6 +95,6 @@ public class ShortestResponseLoadBalance extends AbstractLoadBalance {
                 }
             }
         }
-        return invokers.get(shortestIndexes[ThreadLocalRandom.current().nextInt(shortestCount)]);
+        return invokers.get(shortestIndexes[ThreadLocalRandom.current().nextInt(shortestCount)]); // 如果有多个最短响应相同且权重相同，则随机选择一个invoker
     }
 }
